38e9f9190f6874e9c7353f96775c5368a3104f67
KartikNanda <73540109+KartikNanda@users.noreply.github.com>
2020-12-10 15:39:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var blockers1
+var blockers2
+var blockers3
+var blockers4
+var blockers5
+var blockers6
+var blockers7
+var blockers8
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  blockers1
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
